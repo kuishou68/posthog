ab93c54d1aa5f1818dae591985163b7839f1dcc1
Rodrigo Iloro <rodrigo@posthog.com>
2026-04-24 16:32:35 -0300

    chore(experiments): extract experiment serializers.

--- products/experiments/backend/test/test_presentation_api.py
+++ products/experiments/backend/test/test_presentation_api.py
@@ -5866,7 +5866,7 @@ class TestExperimentParametersFieldMutation(APILicensedTest):
     """
 
     def test_to_internal_value_does_not_mutate_input_dict(self):
-        from products.experiments.backend.presentation.experiments import ExperimentParametersField
+        from products.experiments.backend.presentation.serializers import ExperimentParametersField
 
         input_dict = {
             "feature_flag_variants": [
@@ -5886,7 +5886,7 @@ class TestExperimentParametersFieldMutation(APILicensedTest):
         }
 
     def test_to_representation_does_not_mutate_stored_value(self):
-        from products.experiments.backend.presentation.experiments import ExperimentParametersField
+        from products.experiments.backend.presentation.serializers import ExperimentParametersField
 
         stored_value = {
             "feature_flag_variants": [
