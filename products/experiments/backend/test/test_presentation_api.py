7b80a0d61315944d8ffe837754d486eee2ee95ab
Rodrigo Iloro <rodrigo@posthog.com>
2026-04-24 14:10:16 -0300

    chore(experiment): move experiments viewset and serializers into the product backend folder.

--- products/experiments/backend/test/test_presentation_api.py
+++ products/experiments/backend/test/test_presentation_api.py
@@ -5866,7 +5866,7 @@ class TestExperimentParametersFieldMutation(APILicensedTest):
     """
 
     def test_to_internal_value_does_not_mutate_input_dict(self):
-        from ee.clickhouse.views.experiments import ExperimentParametersField
+        from products.experiments.backend.presentation.experiments import ExperimentParametersField
 
         input_dict = {
             "feature_flag_variants": [
@@ -5886,7 +5886,7 @@ class TestExperimentParametersFieldMutation(APILicensedTest):
         }
 
     def test_to_representation_does_not_mutate_stored_value(self):
-        from ee.clickhouse.views.experiments import ExperimentParametersField
+        from products.experiments.backend.presentation.experiments import ExperimentParametersField
 
         stored_value = {
             "feature_flag_variants": [
